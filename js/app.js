42e866d79be8cb35eed237be8909e2f324a487b5
Zaid Kaleem <zaidkaleem48@gmail.com>
2017-08-15 10:04:51 -0400

    Commented out marker icon for destinations (correction)

--- js/app.js
+++ js/app.js
@@ -71,15 +71,15 @@ function initMap() {
             });
 
             markerArr.push(marker);
-/*
-            marker.setIcon(/** @type {google.maps.Icon} */({
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(35, 35)
-            }));
-*/
+
+//            marker.setIcon(/** @type {google.maps.Icon} */({
+//                url: place.icon,
+//                size: new google.maps.Size(71, 71),
+//                origin: new google.maps.Point(0, 0),
+//                anchor: new google.maps.Point(17, 34),
+//                scaledSize: new google.maps.Size(35, 35)
+//            }));
+
             // marker.setPosition(place.geometry.location);         // fix this shit
            // marker.setVisible(true);
 
